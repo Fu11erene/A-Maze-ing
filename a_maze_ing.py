18f616ff032e029f5b8ba3b726fbbffb4be01399
ktomita <ktomita@student.42tokyo.jp>
2026-08-25 20:28:05 +0900

    add: 迷路の初期化を実装
    盤面をlist[list[bool]]型で初期化し、壁であるかどうかをTrueで表現することで表現するように

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,11 +1,14 @@
 #!/bin/usr/env python3
 
 from src.parser import arg_parse
+from src.mazegen import MazeGenerator
 
 
 def main() -> None:
     config = arg_parse()
     print(config)
+    generator = MazeGenerator(config=config)
+    generator.generate_maze_data()
 
 
 if __name__ == "__main__":
--- src/mazegen/mazegen.py
+++ src/mazegen/mazegen.py
@@ -1,6 +1,50 @@
 from ..parser import Config
 
 
+type Board = list[list[bool]]
+"""
+迷路の盤面を定義する型
+"""
+
+
 class MazeGenerator:
     def __init__(self, config: Config) -> None:
         self.config = config
+
+    def _init_board(self) -> Board:
+        WIDTH = self.config.width * 2 + 1
+        HEIGHT = self.config.height * 2 + 1
+        board = [[False] * (WIDTH)] * (HEIGHT)
+        board = [[False for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+        print("W:", WIDTH)
+        print("H:", HEIGHT)
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                if y == 0 or y == HEIGHT - 1 or x == 0 or x == WIDTH - 1 \
+                        or (y % 2 == 0 and x % 2 == 0):
+                    board[y][x] = True
+                else:
+                    board[y][x] = False
+
+        return board
+
+    @classmethod
+    def _print_board(cls, board: Board):
+        for y, row in enumerate(board):
+            print(y, end=": ")
+            for cell in row:
+                if cell is True:
+                    print("T", end="")
+                else:
+                    print(" ", end="")
+
+            print()
+
+    def generate_maze_data(self):
+        """
+        迷路を生成する
+        """
+
+        board = self._init_board()
+        MazeGenerator._print_board(board)

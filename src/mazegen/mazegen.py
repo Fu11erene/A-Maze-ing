4da067d5e08d28362d6b39f69dfe328cdfae81e0
Kensuke Tomita <106292417+GoRamen-3292@users.noreply.github.com>
2026-08-26 15:10:55 +0900

    MazeGeneratorの不要な処理の削除・実装予定のアルゴリズムについてのコメント追加

--- src/mazegen/mazegen.py
+++ src/mazegen/mazegen.py
@@ -8,24 +8,29 @@ type Board = list[list[bool]]
 
 
 class MazeGenerator:
+    """
+    迷路を生成するジェネレーター
+    """
     def __init__(self, config: Config) -> None:
         self.config = config
 
-    def _init_board(self) -> Board:
+    def _generate_board(self) -> Board:
         WIDTH = self.config.width * 2 + 1
         HEIGHT = self.config.height * 2 + 1
-        board = [[False] * (WIDTH)] * (HEIGHT)
         board = [[False for _ in range(WIDTH)] for _ in range(HEIGHT)]
 
-        print("W:", WIDTH)
-        print("H:", HEIGHT)
         for y in range(HEIGHT):
             for x in range(WIDTH):
                 if y == 0 or y == HEIGHT - 1 or x == 0 or x == WIDTH - 1 \
                         or (y % 2 == 0 and x % 2 == 0):
                     board[y][x] = True
-                else:
-                    board[y][x] = False
+                    # ここに、棒を実際に倒す処理を加える
+                    # 倒す向きの候補を現在何行目であるのかによって判断する
+                    #   1行目なら上下左右
+                    #   2行目...最後なら下と左右、ただし四方を壁に囲われて使わないマスができないようにする
+                    #
+                # else:
+                #     board[y][x] = False
 
         return board
 
@@ -46,5 +51,5 @@ class MazeGenerator:
         迷路を生成する
         """
 
-        board = self._init_board()
+        board = self._generate_board()
         MazeGenerator._print_board(board)

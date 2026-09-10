1637e90afc17d9c4bc5610f97bba39e9e4f7dd52
Fu11erene <tatsuya91uta4@gmail.com>
2026-09-10 14:59:43 +0900

    exit: 例外処理でのexit呼び出しを追加

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,5 +1,6 @@
 #!/bin/usr/env python3
 
+from sys import exit
 from mazegen import MazeGenerator, arg_parse, visualize
 
 
@@ -10,6 +11,7 @@ def main() -> None:
         visualize(maze_gen)
     except ValueError as e:
         print(f"{e.__class__.__name__}: {e}")
+        exit(1)
 
 
 if __name__ == "__main__":
